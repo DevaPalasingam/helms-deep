8bf69951c722010c2373ee297a6d70a2d3fef217
Deva <deva.palasingam@gmail.com>
2023-08-25 15:01:47 -0500

    basic functionality

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Helm's Deep
 
 Like from Lord of the Rings, but slightly less epic. Built with **_Kaboom JS_**.
+
+Background image by gstudioimagen1 on Freepik
--- src/main.js
+++ src/main.js
@@ -1,12 +1,106 @@
-import kaboom from "kaboom"
+import kaboom from "kaboom";
 
-const k = kaboom()
+const SPEED = 150;
+const ORC_FIGHTER_SPEED = 50;
+const BOTTOM = 755;
+const TOP = 590;
+const LEFT_EDGE = 20;
+const RIGHT_EDGE = 1420;
 
-k.loadSprite("bean", "sprites/bean.png")
+kaboom();
 
-k.add([
-	k.pos(120, 80),
-	k.sprite("bean"),
-])
+loadSprite("background", "sprites/background.jpg");
+loadSprite("player", "sprites/hero/walk/tile000.png");
+loadSprite("orc_fighter", "sprites/enemies/orc_fighter/walk/tile000.png");
 
-k.onClick(() => k.addKaboom(k.mousePos()))
+loadSprite("player_attack0", "sprites/hero/attack_1/tile000.png");
+loadSprite("player_attack1", "sprites/hero/attack_1/tile001.png");
+loadSprite("player_attack2", "sprites/hero/attack_1/tile002.png");
+loadSprite("player_attack3", "sprites/hero/attack_1/tile003.png");
+
+scene("game", () => {
+  add([sprite("background", { width: width(), height: height() }, z(0))]);
+  const player = add([
+    sprite("player"),
+    z(1),
+    pos(300, 650),
+    area({ scale: 0.5 }),
+    anchor("bot"),
+    body(),
+  ]);
+
+  //   ------Movement functions----------------
+  onKeyDown("right", () => {
+    if (player.pos.x < RIGHT_EDGE) {
+      player.move(SPEED, 0);
+      player.flipX = false;
+    }
+  });
+  onKeyDown("left", () => {
+    if (player.pos.x > LEFT_EDGE) {
+      player.move(-SPEED, 0);
+      player.flipX = true;
+    }
+  });
+  onKeyDown("up", () => {
+    if (player.pos.y > TOP) {
+      player.move(0, -SPEED);
+    }
+  });
+  onKeyDown("down", () => {
+    if (player.pos.y < BOTTOM) {
+      player.move(0, SPEED);
+    }
+  });
+  //   -------------------------------------
+
+  // -----------Attack Functions-----------------
+  onKeyPress("space", () => {
+    let flipX = player.flipX;
+    player.use(sprite("player_attack0"));
+    player.flipX = flipX;
+    wait(0.1, () => {
+      player.use(sprite("player_attack1"));
+      player.flipX = flipX;
+      wait(0.1, () => {
+        player.use(sprite("player_attack2"));
+        player.flipX = flipX;
+        wait(0.1, () => {
+          player.use(sprite("player_attack3"));
+          player.flipX = flipX;
+          wait(0.1, () => {
+            player.use(sprite("player"));
+            player.flipX = flipX;
+          });
+        });
+      });
+    });
+  });
+  //   ----------------------------------------
+
+  //   -----------Generate Enemies------------------
+  function spawnEnemies() {
+    const enemy = add([
+      area(),
+      pos(width(), rand(TOP, BOTTOM)),
+      anchor("bot"),
+      move(LEFT, ORC_FIGHTER_SPEED),
+      body(),
+      area({ scale: 0.5 }),
+      sprite("orc_fighter"),
+      "orc_fighter",
+    ]);
+    enemy.flipX = true;
+
+    onKeyPress("space", () => {
+      if (enemy.pos.dist(player.pos) < 70) {
+        destroy(enemy);
+      }
+    });
+    // wait a random amount of time to spawn next bean
+    wait(rand(0.7, 2), spawnEnemies);
+  }
+  spawnEnemies();
+});
+
+go("game");
