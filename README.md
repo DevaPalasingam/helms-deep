7a5cdfb86e21e70d1df31e446e13b51eb15ca6d2
Deva <deva.palasingam@gmail.com>
2023-08-25 16:35:06 -0500

    need to figure out door functionality

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Like from Lord of the Rings, but slightly less epic. Built with **_Kaboom JS_**.
 
 Background image by gstudioimagen1 on Freepik
+
+## How to play
+
+- Protect the entrance to Helm's Deep
+- Use the arrow keys to move
+- Press spacebar to attack
--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ const BOTTOM = 755;
 const TOP = 590;
 const LEFT_EDGE = 20;
 const RIGHT_EDGE = 1420;
+const DOOR_LEFT = 500;
+const DOOR_RIGHT = 900;
 
 kaboom();
 
@@ -18,12 +20,17 @@ loadSprite("player_attack1", "sprites/hero/attack_1/tile001.png");
 loadSprite("player_attack2", "sprites/hero/attack_1/tile002.png");
 loadSprite("player_attack3", "sprites/hero/attack_1/tile003.png");
 
+loadSprite("orc_fighter_dead0", "sprites/enemies/orc_fighter/dead/tile000.png");
+loadSprite("orc_fighter_dead1", "sprites/enemies/orc_fighter/dead/tile001.png");
+loadSprite("orc_fighter_dead2", "sprites/enemies/orc_fighter/dead/tile002.png");
+loadSprite("orc_fighter_dead3", "sprites/enemies/orc_fighter/dead/tile003.png");
+
 scene("game", () => {
   add([sprite("background", { width: width(), height: height() }, z(0))]);
   const player = add([
     sprite("player"),
     z(1),
-    pos(300, 650),
+    pos(900, 650),
     area({ scale: 0.5 }),
     anchor("bot"),
     body(),
@@ -80,27 +87,62 @@ scene("game", () => {
 
   //   -----------Generate Enemies------------------
   function spawnEnemies() {
+    let randSpawn = rand(0, 2);
+    let spawnPoint = 0;
+    let enemyMove = RIGHT;
+    let enemyFlip = false;
+    if (randSpawn > 1) {
+      spawnPoint = width();
+      enemyMove = LEFT;
+      enemyFlip = true;
+    }
+
     const enemy = add([
       area(),
-      pos(width(), rand(TOP, BOTTOM)),
+      pos(spawnPoint, rand(TOP, BOTTOM)),
       anchor("bot"),
-      move(LEFT, ORC_FIGHTER_SPEED),
+      move(enemyMove, ORC_FIGHTER_SPEED),
       body(),
       area({ scale: 0.5 }),
       sprite("orc_fighter"),
       "orc_fighter",
     ]);
-    enemy.flipX = true;
+    enemy.flipX = enemyFlip;
 
+    // --------Enemy Death Sequence-------------------
     onKeyPress("space", () => {
-      if (enemy.pos.dist(player.pos) < 70) {
-        destroy(enemy);
-      }
+      wait(0.2, () => {
+        if (enemy.pos.dist(player.pos) < 70) {
+          let flipX = enemy.flipX;
+          wait(0.1, () => {
+            enemy.use(sprite("orc_fighter_dead0"));
+            enemy.flipX = flipX;
+            wait(0.1, () => {
+              enemy.use(sprite("orc_fighter_dead1"));
+              enemy.flipX = flipX;
+              wait(0.1, () => {
+                enemy.use(sprite("orc_fighter_dead2"));
+                enemy.flipX = flipX;
+                wait(0.1, () => {
+                  enemy.use(sprite("orc_fighter_dead3"));
+                  enemy.flipX = flipX;
+                  wait(0.1, () => {
+                    destroy(enemy);
+                  });
+                });
+              });
+            });
+          });
+        }
+      });
     });
+    // --------------------------------------
+
     // wait a random amount of time to spawn next bean
     wait(rand(0.7, 2), spawnEnemies);
   }
   spawnEnemies();
+  //   ------------------------------------------------------------
 });
 
 go("game");
