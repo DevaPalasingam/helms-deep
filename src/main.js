3e338a2b70bbc703d07e4da538e0e4d98027b5af
Deva <deva.palasingam@gmail.com>
2023-08-27 10:43:13 -0500

    adding other enemies

--- src/main.js
+++ src/main.js
@@ -14,6 +14,8 @@ kaboom();
 loadSprite("background", "sprites/background.jpg");
 loadSprite("player", "sprites/hero/walk/tile000.png");
 loadSprite("orc_fighter", "sprites/enemies/orc_fighter/walk/tile000.png");
+loadSprite("orc_runner", "sprites/enemies/orc_runner/walk/tile000.png");
+loadSprite("orc_shaman", "sprites/enemies/orc_shaman/walk/tile000.png");
 
 loadSprite("player_attack0", "sprites/hero/attack_1/tile000.png");
 loadSprite("player_attack1", "sprites/hero/attack_1/tile001.png");
@@ -25,11 +27,36 @@ loadSprite("orc_fighter_dead1", "sprites/enemies/orc_fighter/dead/tile001.png");
 loadSprite("orc_fighter_dead2", "sprites/enemies/orc_fighter/dead/tile002.png");
 loadSprite("orc_fighter_dead3", "sprites/enemies/orc_fighter/dead/tile003.png");
 
+loadSprite("orc_runner_dead0", "sprites/enemies/orc_runner/dead/tile000.png");
+loadSprite("orc_runner_dead1", "sprites/enemies/orc_runner/dead/tile001.png");
+
+loadSprite("orc_shaman_dead0", "sprites/enemies/orc_shaman/dead/tile000.png");
+loadSprite("orc_shaman_dead1", "sprites/enemies/orc_shaman/dead/tile001.png");
+loadSprite("orc_shaman_dead2", "sprites/enemies/orc_shaman/dead/tile002.png");
+loadSprite("orc_shaman_dead3", "sprites/enemies/orc_shaman/dead/tile003.png");
+loadSprite("orc_shaman_dead3", "sprites/enemies/orc_shaman/dead/tile004.png");
+
 loadSprite("orc_fighter_jump0", "sprites/enemies/orc_fighter/jump/tile000.png");
 loadSprite("orc_fighter_jump1", "sprites/enemies/orc_fighter/jump/tile001.png");
 loadSprite("orc_fighter_jump2", "sprites/enemies/orc_fighter/jump/tile002.png");
 loadSprite("orc_fighter_jump3", "sprites/enemies/orc_fighter/jump/tile003.png");
 loadSprite("orc_fighter_jump4", "sprites/enemies/orc_fighter/jump/tile004.png");
+
+loadSprite("orc_runner_jump0", "sprites/enemies/orc_runner/jump/tile000.png");
+loadSprite("orc_runner_jump1", "sprites/enemies/orc_runner/jump/tile001.png");
+loadSprite("orc_runner_jump2", "sprites/enemies/orc_runner/jump/tile002.png");
+loadSprite("orc_runner_jump3", "sprites/enemies/orc_runner/jump/tile003.png");
+loadSprite("orc_runner_jump4", "sprites/enemies/orc_runner/jump/tile004.png");
+loadSprite("orc_runner_jump5", "sprites/enemies/orc_runner/jump/tile005.png");
+loadSprite("orc_runner_jump6", "sprites/enemies/orc_runner/jump/tile006.png");
+loadSprite("orc_runner_jump7", "sprites/enemies/orc_runner/jump/tile007.png");
+
+loadSprite("orc_shaman_magic0", "sprites/enemies/orc_shaman/magic/tile000.png");
+loadSprite("orc_shaman_magic1", "sprites/enemies/orc_shaman/magic/tile001.png");
+loadSprite("orc_shaman_magic2", "sprites/enemies/orc_shaman/magic/tile002.png");
+loadSprite("orc_shaman_magic3", "sprites/enemies/orc_shaman/magic/tile003.png");
+loadSprite("orc_shaman_magic4", "sprites/enemies/orc_shaman/magic/tile004.png");
+loadSprite("orc_shaman_magic5", "sprites/enemies/orc_shaman/magic/tile005.png");
 
 scene("game", () => {
   add([sprite("background", { width: width(), height: height() }, z(0))]);
@@ -92,7 +119,7 @@ scene("game", () => {
   //   ----------------------------------------
 
   //   -----------Generate Enemies------------------
-  function spawnEnemies() {
+  function spawnFighters() {
     let randSpawn = rand(0, 2);
     let spawnPoint = 0;
     let enemyMove = ORC_FIGHTER_SPEED;
@@ -192,9 +219,112 @@ scene("game", () => {
     // --------------------------------------
 
     // wait a random amount of time to spawn next bean
-    wait(rand(0.7, 2), spawnEnemies);
+    wait(rand(0.7, 2), spawnFighters);
   }
-  spawnEnemies();
+  // TODO: SpawnRunners Function
+  function spawnRunners() {
+    let randSpawn = rand(0, 2);
+    let spawnPoint = 0;
+    let enemyMove = ORC_FIGHTER_SPEED;
+    let enemyFlip = false;
+    if (randSpawn > 1) {
+      spawnPoint = width();
+      enemyMove = -ORC_FIGHTER_SPEED;
+      enemyFlip = true;
+    }
+
+    const enemy = add([
+      area(),
+      pos(spawnPoint, rand(TOP, BOTTOM)),
+      anchor("bot"),
+      body(),
+      area({ scale: 0.5 }),
+      sprite("orc_fighter"),
+      state("run", ["run", "getClose", "attack"]),
+      "orc_fighter",
+    ]);
+    enemy.flipX = enemyFlip;
+
+    // ----------Orc States--------------------
+    let attackState = false;
+    enemy.onStateUpdate("run", () => {
+      enemy.move(enemyMove, 0);
+      if (enemy.pos.x > DOOR_LEFT && enemy.pos.x < DOOR_RIGHT) {
+        enemy.enterState("getClose");
+      }
+    });
+    enemy.onStateUpdate("getClose", () => {
+      enemy.move(0, -ORC_FIGHTER_SPEED);
+      if (enemy.pos.y <= TOP) {
+        enemy.enterState("attack");
+      }
+    });
+    enemy.onStateUpdate("attack", () => {
+      if (!attackState) {
+        attackState = true;
+        wait(0.1, () => {
+          enemy.use(sprite("orc_fighter_jump0"));
+          enemy.flip = enemyFlip;
+          wait(0.1, () => {
+            enemy.use(sprite("orc_fighter_jump1"));
+            enemy.flip = enemyFlip;
+            wait(0.1, () => {
+              enemy.use(sprite("orc_fighter_jump2"));
+              enemy.flip = enemyFlip;
+              wait(0.1, () => {
+                enemy.use(sprite("orc_fighter_jump3"));
+                enemy.flip = enemyFlip;
+                wait(0.1, () => {
+                  enemy.use(sprite("orc_fighter_jump4"));
+                  enemy.flip = enemyFlip;
+                  wait(0.1, () => {
+                    addKaboom(enemy.pos);
+                    shake();
+                    destroy(enemy);
+                  });
+                });
+              });
+            });
+          });
+        });
+      }
+    });
+    // Orc States
+    // ---------------------------------------
+
+    // --------Enemy Death Sequence-------------------
+    onKeyPress("space", () => {
+      wait(0.2, () => {
+        if (enemy.pos.dist(player.pos) < 70) {
+          let flipX = enemy.flipX;
+          wait(0.1, () => {
+            enemy.use(sprite("orc_fighter_dead0"));
+            enemy.flipX = flipX;
+            wait(0.1, () => {
+              enemy.use(sprite("orc_fighter_dead1"));
+              enemy.flipX = flipX;
+              wait(0.1, () => {
+                enemy.use(sprite("orc_fighter_dead2"));
+                enemy.flipX = flipX;
+                wait(0.1, () => {
+                  enemy.use(sprite("orc_fighter_dead3"));
+                  enemy.flipX = flipX;
+                  wait(0.1, () => {
+                    destroy(enemy);
+                  });
+                });
+              });
+            });
+          });
+        }
+      });
+    });
+    // --------------------------------------
+
+    // wait a random amount of time to spawn next bean
+    wait(rand(0.7, 2), spawnRunners);
+  }
+  spawnFighters();
   //   ------------------------------------------------------------
 });
 
